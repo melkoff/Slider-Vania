64a5e2c8946be52d7860eaa661f49000d99b1315
melkoff <vasamelko@gmail.com>
2023-07-04 12:38:45 +0300

    Slider for review maybe

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,25 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
+var slideIndex = 0;
+var slides = document.getElementsByClassName("slider-wrapper")[0].children;
+
+function showSlide(n) {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+
+  slideIndex = (n + slides.length) % slides.length;
+  slides[slideIndex].style.opacity = 1;
+}
+
+function changeSlide(n) {
+  showSlide(slideIndex + n);
+}
+
+document.getElementsByClassName("slider-prev")[0].addEventListener("click", function () {
+  changeSlide(-1);
 });
 
+document.getElementsByClassName("slider-next")[0].addEventListener("click", function () {
+  changeSlide(1);
+});
+
+showSlide(slideIndex);
