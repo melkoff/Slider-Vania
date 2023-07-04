f5eb051860cb597270770d8114ee1a7052f3051c
melkoff <vasamelko@gmail.com>
2023-07-04 12:50:20 +0300

    Good review Slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
 var slideIndex = 0;
-var slides = document.getElementsByClassName("slider-wrapper")[0].children;
+var slides = document.getElementsByClassName("slider-wrapper-review")[0].children;
+var totalSlides = slides.length;
 
 function showSlide(n) {
-  for (var i = 0; i < slides.length; i++) {
+  for (var i = 0; i < totalSlides; i++) {
     slides[i].style.opacity = 0;
   }
 
-  slideIndex = (n + slides.length) % slides.length;
+  slideIndex = (n + totalSlides) % totalSlides;
   slides[slideIndex].style.opacity = 1;
 }
 
@@ -20,6 +21,23 @@ document.getElementsByClassName("slider-prev")[0].addEventListener("click", func
 
 document.getElementsByClassName("slider-next")[0].addEventListener("click", function () {
   changeSlide(1);
+});
+
+function rotateSlides() {
+  changeSlide(1);
+}
+
+// Запуск автоматичного переходу між слайдами
+var interval = setInterval(rotateSlides, 3000);
+
+// Зупинка автоматичного переходу при наведенні курсора на слайдер
+document.getElementsByClassName("slider-container-review")[0].addEventListener("mouseenter", function () {
+  clearInterval(interval);
+});
+
+// Поновлення автоматичного переходу після зняття курсора зі слайдера
+document.getElementsByClassName("slider-container-review")[0].addEventListener("mouseleave", function () {
+  interval = setInterval(rotateSlides, 3000);
 });
 
 showSlide(slideIndex);
