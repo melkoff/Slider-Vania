8420e4360d54d4d2b96f328ec343484c6947c1af
KovbaIvanPc <kovbaivan11112@gmail.com>
2024-01-09 01:07:16 +0200

    add changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,68 +1,121 @@
-//question popup*************************************************************
-let questionPopup = document.getElementById('question-popup');
-let questionPopupClose = document.querySelector('.popup-close');
+window.addEventListener('DOMContentLoaded', () => {
+	//question popup*************************************************************
+	let questionPopup = document.getElementById('question-popup');
+	let questionPopupClose = document.querySelector('.popup-close');
 
-let questBtn = document.getElementById('questBtn');
+	let questBtn = document.querySelectorAll('#questBtn');
 
-questBtn.onclick = function () {
-	questionPopup.classList.toggle('_active');
-};
-questionPopupClose.onclick = function () {
-	questionPopup.classList.toggle('_active');
-};
-// question popup end*************************************************************
-// =========================================================================================== //
-// burger menu ************************************************************
-const burgerTxtMenu = document.getElementById('burgerTextOpen');
-const burgerTxtClose = document.getElementById('burgerTextClose');
-const menu = document.getElementById('mainMenu');
-const btnBurger = document.getElementById('burgerMenu');
-
-const headerContacts = document.querySelector('.header__container_contacts');
-const headerContainerMenu = document.querySelector('.header__container_menu');
-const headerLogo = document.querySelector('.header__logo');
-
-btnBurger.onclick = function () {
-	menu.classList.toggle('_active');
-	btnBurger.classList.toggle('_active');
-	burgerTxtMenu.classList.toggle('_active');
-	burgerTxtClose.classList.toggle('_active');
-	if (innerWidth > 1200) {
-		headerContacts.classList.toggle('_active');
-		headerContainerMenu.classList.toggle('_active');
-	}
-	headerLogo.classList.toggle('_active');
-	// change logo 	*************************************************************
-
-	if (headerLogo.classList.contains('_active')) {
-		headerLogo.src = './img/logo_white.svg';
-	} else {
-		headerLogo.src = './img/logo.svg';
-	}
-
-	// change logo end***********************************************************
-};
-// burger menu end ************************************************************
-// sublist menu ************************************************************
-const sublistMenuPortfolioBtn = document.querySelector('#menu-sublist-portfolio-btn');
-const sublistMenuPortfolio = document.querySelector('#menu-sublist-portfolio');
-const sublistArrowClose = document.querySelectorAll('#sublist-arrow-close');
-// sublist menu services
-const sublistMenuServices = document.querySelector('#menu-sublist-services');
-const sublistMenuServicesBtn = document.querySelector('#menu-sublist-services-btn');
-
-sublistMenuPortfolioBtn.onclick = function () {
-	sublistMenuPortfolio.classList.toggle('_active');
-};
-sublistMenuServicesBtn.onclick = function () {
-	sublistMenuServices.classList.toggle('_active');
-};
-
-sublistArrowClose.forEach(function (close) {
-	close.onclick = function () {
-		sublistMenuPortfolio.classList.remove('_active');
-		sublistMenuServices.classList.remove('_active');
+	questBtn.forEach(function (btn) {
+		btn.onclick = function () {
+			questionPopup.classList.toggle('_active');
+		};
+	});
+	questionPopupClose.onclick = function () {
+		questionPopup.classList.toggle('_active');
 	};
-});
+	// question popup end*************************************************************
+	// =========================================================================================== //
+	// burger menu ************************************************************
+	const burgerTxtMenu = document.getElementById('burgerTextOpen');
+	const burgerTxtClose = document.getElementById('burgerTextClose');
+	const menu = document.getElementById('mainMenu');
+	const btnBurger = document.getElementById('burgerMenu');
+	const btnBurgerClose = document.querySelector('.top-menu__close');
 
-// sublist menu close ************************************************************
+	const headerContacts = document.querySelector('.header__container_contacts');
+	const headerContainerMenu = document.querySelector('.header__container_menu');
+	const headerLogo = document.querySelector('.header__logo');
+
+	if (window.innerWidth > 968) {
+		btnBurger.onclick = function () {
+			menu.classList.toggle('_active');
+			btnBurger.classList.toggle('_active');
+			burgerTxtMenu.classList.toggle('_active');
+			burgerTxtClose.classList.toggle('_active');
+			if (innerWidth > 1200) {
+				headerContacts.classList.toggle('_active');
+				headerContainerMenu.classList.toggle('_active');
+			}
+			headerLogo.classList.toggle('_active');
+			// change logo 	*************************************************************
+
+			if (headerLogo.classList.contains('_active')) {
+				headerLogo.src = './img/logo_white.svg';
+			} else {
+				headerLogo.src = './img/logo.svg';
+			}
+			// change logo end***********************************************************
+		};
+	}
+	if (window.innerWidth <= 968) {
+		btnBurger.onclick = function () {
+			menu.classList.toggle('_active');
+		};
+		btnBurgerClose.onclick = function () {
+			menu.classList.toggle('_active');
+		};
+	}
+	// burger menu end ************************************************************
+	// sublist menu ************************************************************
+
+	const sublistMenuPortfolioBtn = document.querySelector('#menu-sublist-portfolio-btn');
+	const sublistMenuPortfolio = document.querySelector('#menu-sublist-portfolio');
+	const sublistArrowClose = document.querySelectorAll('#sublist-arrow-close');
+	// sublist menu services
+	const sublistMenuServices = document.querySelector('#menu-sublist-services');
+	const sublistMenuServicesBtn = document.querySelector('#menu-sublist-services-btn');
+	if (window.innerWidth > 968) {
+		sublistMenuPortfolioBtn.onclick = function () {
+			sublistMenuPortfolio.classList.toggle('_active');
+		};
+		sublistMenuServicesBtn.onclick = function () {
+			sublistMenuServices.classList.toggle('_active');
+		};
+
+		sublistArrowClose.forEach(function (close) {
+			close.onclick = function () {
+				sublistMenuPortfolio.classList.remove('_active');
+				sublistMenuServices.classList.remove('_active');
+			};
+		});
+	}
+
+	// sublist menu close ************************************************************
+	// accordion ************************************************************
+	let activeAccordion = null;
+
+	function closeAccordion() {
+		if (activeAccordion) {
+			activeAccordion.classList.remove('active');
+			const panel = activeAccordion.nextElementSibling;
+			panel.style.maxHeight = null;
+			const checkbox = activeAccordion.querySelector('input[type="radio"]');
+			activeAccordion = null;
+		}
+	}
+
+	function accordion(accordionBtn, accordionPanel) {
+		const accordions = document.querySelectorAll(accordionBtn);
+
+		accordions.forEach(accordion => {
+			accordion.addEventListener('click', function (e) {
+				e.preventDefault();
+				const panel = this.nextElementSibling; // Get the sibling (accordion content) element
+				const isActive = this.classList.contains('active');
+
+				closeAccordion(); // Close the currently active accordion (if any)
+
+				if (!isActive) {
+					this.classList.add('active');
+					panel.classList.add('active');
+					panel.style.maxHeight = panel.scrollHeight + 'px';
+					activeAccordion = this; // Set the current accordion as the active one
+				}
+			});
+		});
+	}
+
+	accordion('#spoller1Title', '#spoller1Body');
+	accordion('#spoller2Title', '#spoller2Body');
+	// accrodion end ************************************************************
+});
