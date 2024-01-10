46399131a66a772c50e00baf9b4d8e961cf45c82
KovbaIvanPc <kovbaivan11112@gmail.com>
2024-01-10 15:42:33 +0200

    start mouseover

--- src/js/script.js
+++ src/js/script.js
@@ -1,108 +1,142 @@
 window.addEventListener('DOMContentLoaded', () => {
+	$('.projects__card-inner').on('mousemove', function (e) {
+		var circle = $(this).find('.projects__circle');
+		var icon = $(this).find('.projects__circle-icon');
+
+		var mouseX = e.pageX - $(this).offset().left;
+		var mouseY = e.pageY - $(this).offset().top;
+
+		gsap.to(circle, { opacity: 1, duration: 0.3 });
+		gsap.to(icon, { x: mouseX, y: mouseY, duration: 0.3 });
+	});
+
+	$('.projects__card-inner').on('mouseleave', function () {
+		var circle = $(this).find('.projects__circle');
+		gsap.to(circle, { opacity: 0, duration: 0.3 });
+	});
+	// jquery functions ************************************************************
+
+	// jquery functions end*************************************************************
 	// animation ************************************************************
-	const fadeInUpElements = document.querySelectorAll('.fadeInUp');
+	const fadeUpAnimation = () => {
+		const fadeInUpElements = document.querySelectorAll('.fadeInUp');
 
-	function isInViewport(element) {
-		const rect = element.getBoundingClientRect();
-		return (
-			rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-		);
-	}
+		function isInViewport(element) {
+			const rect = element.getBoundingClientRect();
+			return (
+				rect.top >= 0 &&
+				rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+			);
+		}
 
-	function handleScroll() {
-		fadeInUpElements.forEach(function (element) {
-			if (isInViewport(element)) {
-				element.classList.add('fadeInUp');
-			}
-		});
-	}
+		function handleScroll() {
+			fadeInUpElements.forEach(function (element) {
+				if (isInViewport(element)) {
+					element.classList.add('fadeInUp');
+				}
+			});
+		}
 
-	// Initial check on page load
-	handleScroll();
+		// Initial check on page load
+		handleScroll();
 
-	// Check when the user scrolls
-	window.addEventListener('scroll', handleScroll);
+		// Check when the user scrolls
+		window.addEventListener('scroll', handleScroll);
+	};
+	fadeUpAnimation();
+
 	// animation end ************************************************************
 	//question popup*************************************************************
-	let questionPopup = document.getElementById('question-popup');
-	let questionPopupClose = document.querySelector('.popup-close');
+	const questPopupFunction = () => {
+		let questionPopup = document.getElementById('question-popup');
+		let questionPopupClose = document.querySelector('.popup-close');
 
-	let questBtn = document.querySelectorAll('#questBtn');
+		let questBtn = document.querySelectorAll('#questBtn');
 
-	questBtn.forEach(function (btn) {
-		btn.onclick = function () {
-			questionPopup.classList.toggle('_active');
-		};
-	});
-	questionPopupClose.onclick = function () {
-		questionPopup.classList.toggle('_active');
-	};
-	// question popup end*************************************************************
-	// =========================================================================================== //
-	// burger menu ************************************************************
-	const burgerTxtMenu = document.getElementById('burgerTextOpen');
-	const burgerTxtClose = document.getElementById('burgerTextClose');
-	const menu = document.getElementById('mainMenu');
-	const btnBurger = document.getElementById('burgerMenu');
-	const btnBurgerClose = document.querySelector('.top-menu__close');
-
-	const headerContacts = document.querySelector('.header__container_contacts');
-	const headerContainerMenu = document.querySelector('.header__container_menu');
-	const headerLogo = document.querySelector('.header__logo');
-
-	if (window.innerWidth > 968) {
-		btnBurger.onclick = function () {
-			menu.classList.toggle('_active');
-			btnBurger.classList.toggle('_active');
-			burgerTxtMenu.classList.toggle('_active');
-			burgerTxtClose.classList.toggle('_active');
-			if (innerWidth > 1200) {
-				headerContacts.classList.toggle('_active');
-				headerContainerMenu.classList.toggle('_active');
-			}
-			headerLogo.classList.toggle('_active');
-			// change logo 	*************************************************************
-
-			if (headerLogo.classList.contains('_active')) {
-				headerLogo.src = './img/logo_white.svg';
-			} else {
-				headerLogo.src = './img/logo.svg';
-			}
-			// change logo end***********************************************************
-		};
-	}
-	if (window.innerWidth <= 968) {
-		btnBurger.onclick = function () {
-			menu.classList.toggle('_active');
-		};
-		btnBurgerClose.onclick = function () {
-			menu.classList.toggle('_active');
-		};
-	}
-	// burger menu end ************************************************************
-	// sublist menu ************************************************************
-
-	const sublistMenuPortfolioBtn = document.querySelector('#menu-sublist-portfolio-btn');
-	const sublistMenuPortfolio = document.querySelector('#menu-sublist-portfolio');
-	const sublistArrowClose = document.querySelectorAll('#sublist-arrow-close');
-	// sublist menu services
-	const sublistMenuServices = document.querySelector('#menu-sublist-services');
-	const sublistMenuServicesBtn = document.querySelector('#menu-sublist-services-btn');
-	if (window.innerWidth > 968) {
-		sublistMenuPortfolioBtn.onclick = function () {
-			sublistMenuPortfolio.classList.toggle('_active');
-		};
-		sublistMenuServicesBtn.onclick = function () {
-			sublistMenuServices.classList.toggle('_active');
-		};
-
-		sublistArrowClose.forEach(function (close) {
-			close.onclick = function () {
-				sublistMenuPortfolio.classList.remove('_active');
-				sublistMenuServices.classList.remove('_active');
+		questBtn.forEach(function (btn) {
+			btn.onclick = function () {
+				questionPopup.classList.toggle('_active');
 			};
 		});
-	}
+		questionPopupClose.onclick = function () {
+			questionPopup.classList.toggle('_active');
+		};
+	};
+	questPopupFunction();
+
+	// question popup end*************************************************************
+
+	const burgerFunction = () => {
+		// burger menu ************************************************************
+		const burgerTxtMenu = document.getElementById('burgerTextOpen');
+		const burgerTxtClose = document.getElementById('burgerTextClose');
+		const menu = document.getElementById('mainMenu');
+		const btnBurger = document.getElementById('burgerMenu');
+		const btnBurgerClose = document.querySelector('.top-menu__close');
+
+		const headerContacts = document.querySelector('.header__container_contacts');
+		const headerContainerMenu = document.querySelector('.header__container_menu');
+		const headerLogo = document.querySelector('.header__logo');
+
+		if (window.innerWidth > 968) {
+			btnBurger.onclick = function () {
+				menu.classList.toggle('_active');
+				btnBurger.classList.toggle('_active');
+				burgerTxtMenu.classList.toggle('_active');
+				burgerTxtClose.classList.toggle('_active');
+				if (innerWidth > 1200) {
+					headerContacts.classList.toggle('_active');
+					headerContainerMenu.classList.toggle('_active');
+				}
+				headerLogo.classList.toggle('_active');
+				// change logo 	*************************************************************
+
+				if (headerLogo.classList.contains('_active')) {
+					headerLogo.src = './img/logo_white.svg';
+				} else {
+					headerLogo.src = './img/logo.svg';
+				}
+				// change logo end***********************************************************
+			};
+		}
+		if (window.innerWidth <= 968) {
+			btnBurger.onclick = function () {
+				menu.classList.toggle('_active');
+			};
+			btnBurgerClose.onclick = function () {
+				menu.classList.toggle('_active');
+			};
+		}
+		// burger menu end ************************************************************
+	};
+	burgerFunction();
+	// =========================================================================================== //
+
+	// sublist menu ************************************************************
+	const subMenuDesctopFunction = () => {
+		const sublistMenuPortfolioBtn = document.querySelector('#menu-sublist-portfolio-btn');
+		const sublistMenuPortfolio = document.querySelector('#menu-sublist-portfolio');
+		const sublistArrowClose = document.querySelectorAll('#sublist-arrow-close');
+		// sublist menu services
+		const sublistMenuServices = document.querySelector('#menu-sublist-services');
+		const sublistMenuServicesBtn = document.querySelector('#menu-sublist-services-btn');
+		if (window.innerWidth > 968) {
+			sublistMenuPortfolioBtn.onclick = function () {
+				sublistMenuPortfolio.classList.toggle('_active');
+			};
+			sublistMenuServicesBtn.onclick = function () {
+				sublistMenuServices.classList.toggle('_active');
+			};
+
+			sublistArrowClose.forEach(function (close) {
+				close.onclick = function () {
+					sublistMenuPortfolio.classList.remove('_active');
+					sublistMenuServices.classList.remove('_active');
+				};
+			});
+		}
+	};
+	subMenuDesctopFunction();
 
 	// sublist menu close ************************************************************
 	// accordion ************************************************************
@@ -142,4 +176,63 @@ window.addEventListener('DOMContentLoaded', () => {
 	accordion('#spoller1Title', '#spoller1Body');
 	accordion('#spoller2Title', '#spoller2Body');
 	// accrodion end ************************************************************
+
+	const heroSupnumber = new SplitType('#heroSupnumber', { charsClass: 'char' });
+	const heroNumber = new SplitType('#heroNumber', { charsClass: 'char' });
+	const heroTitle = new SplitType('.hero__title', { charsClass: 'char' });
+	const heroBtnChars = new SplitType('.hero__btn', { charsClass: 'char' });
+	// const heroBtn = document.querySelector('.hero__btn');
+
+	gsap.from(heroSupnumber.chars, {
+		opacity: 0,
+		y: -20,
+		duration: 0.2,
+		stagger: 0.05,
+	});
+	gsap.from(heroNumber.chars, {
+		opacity: 0,
+		y: -100,
+		duration: 1,
+		stagger: 0.5,
+	});
+
+	// Create a new timeline
+	const timeline = gsap.timeline();
+
+	// Add the 'from' part of the animation to the timeline
+	timeline.from(heroTitle.chars, {
+		opacity: 0,
+		x: 10,
+		duration: 1,
+		stagger: 0.08,
+	});
+	timeline.to(heroTitle.chars, {
+		opacity: 1,
+		x: 0,
+		duration: 1,
+		stagger: 0.08,
+	});
+
+	timeline.from(heroBtnChars.chars, {
+		opacity: 0,
+		x: 10,
+		duration: 1,
+		stagger: 0.08,
+	});
+	timeline.to(
+		heroBtnChars.chars,
+		{
+			opacity: 1,
+			x: 0,
+			duration: 1,
+			stagger: 0.08,
+		},
+		0,
+	);
+	gsap.to('.projects__circle-icon', {
+		duration: 0.3,
+		x: mouseX,
+		y: mouseY,
+		ease: 'power2.out',
+	});
 });
